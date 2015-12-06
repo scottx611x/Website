debb3c0f22d46ef1bf5bebeda8cc5bce6deb39aa
root <scottx611x@gmail.com>
2015-12-06 22:03:52 +0000

    Removed lots of stuff

--- wget_dl.py
+++
@@ -1,8 +0,0 @@
-import sys,os
-
-BAC = str(sys.argv[1])
-os.system("wget -P %s/ ftp://ftp.ncbi.nih.gov/genomes/Bacteria/%s/*.ptt" % (BAC,BAC))
-os.system("wget -P %s/ ftp://ftp.ncbi.nih.gov/genomes/Bacteria/%s/*.fna" % (BAC,BAC))
-with open("DL_TESTER","w") as DL:
-	DL.write("wget -P %s/ ftp://ftp.ncbi.nih.gov/genomes/Bacteria/%s/*.ptt" % (BAC,BAC))
-
