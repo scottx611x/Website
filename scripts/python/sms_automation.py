520e7d7cd802ecd2b5b63a304401c4fe741d36a9
root <scottx611x@gmail.com>
2016-02-14 15:25:28 -0500

    Modified pizza ordering script

--- scripts/python/sms_automation.py
+++ scripts/python/sms_automation.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-# Scott Ouellette 2-12-1016
-# Pizza ordering script
-# Send a 🍕  and order your pizza for ParkLab meetings automatically
+# Scott Ouellette 2-14-1016
+# Automate various tasks by sending text messages to a Flask server
+
+# For Example: Send a 🍕  and order your pizza for ParkLab meetings automatically
 
 from __future__ import print_function
 import os
@@ -9,24 +10,25 @@ import re
 import sys
 import urllib2
 import smtplib
-from flask import Flask, request, redirect
+import datetime
 import twilio.twiml
 from twilio.rest import TwilioRestClient
+from flask import Flask, request, redirect
 from flask_phantom_emoji import PhantomEmoji
 
+# Read in credentials from config file
 with open('/var/www/html/Website/scripts/python/config') as f:
       credentials = [x.strip() for x in f.readlines()]
       account = credentials[3]
       token = credentials[4]
       client = TwilioRestClient(account, token)
 
+# Instantiate Flask app and allow handling of emojis
 app = Flask(__name__)
 PhantomEmoji(app)
 
 @app.route("/", methods=['GET', 'POST'])
-def order_pizza():
-    """Orders pizza based on sender's "user" status and if the GET body has a pizza emoji"""
-    
+def handle_request():
     users = {"+12075136000": "Scott Ouellette"}
     from_number = request.values.get('From')
     body = unicode(request.values.get("Body"))
@@ -34,11 +36,38 @@ def order_pizza():
     if from_number in users:
         name = users[from_number]
         
+        # If theres a pizza emoji in the HTTP request
         if u"\U0001F355" in body:    
             try:
+                d = datetime.date.today()
+
+                while d.weekday() != 4:                                                                                  d += datetime.timedelta(1)
+
                 fromaddr = 'scottx611x@gmail.com'
                 toaddrs  = 'scottx611x@gmail.com'
-                msg = 'Pizza please?'
+                cc = ['Scott_Ouellette@hms.harvard.edu']
+		
+		msg = "Subject: Pizza Order" 
+                msg +=  "CC: %s\r\n" % ",".join(cc)
+                msg += '''
+Hello,
+
+I’d like to place an order to be delivered between 11:30-11:45 to the 4th floor of Countway Library at Harvard Medical School on Friday, {}:
+  
+1 Large Cheese
+1 Large Buffalo Chicken & Bacon w/ blue cheese
+1 Large Veggie
+1 Large Supreme
+1 Large Margarita
+1 2-liter diet Pepsi
+ 
+You should have Aimee Smith’s (cc’d) credit card information on file.
+ 
+If you have any questions please let me know!
+ 
+Best,
+Scott Ouellette
+'''.format(d.strftime('%m/%d'))
 
                 username = 'scottx611x@gmail.com'
                 password = credentials[5]
@@ -49,8 +78,8 @@ def order_pizza():
                 server.login(username,password)
                 server.sendmail(fromaddr, toaddrs, msg)
                 server.quit()
-                message = "Thanks %s, your Pizza Order has been sent! :)" % name
-                client.messages.create(to="+12075136000", from_="+15106626969", body=message)
+                message = "Thanks %s, your Pizza Order has been sent! %s%s%s" % (name, u"\U0001F355", u"\U0001F355", u"\U0001F355")
+                client.messages.create(to=from_number, from_="+15106626969", body=message)
             
             except Exception as e:
                 with open("srv_log", "w+") as f:
