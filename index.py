9bbf8e414754067c146c88e7a82e402b027a12b6
Scott Ouellette <scottx611x@gmail.com>
2017-05-22 14:11:25 -0700

    Choose random template on site access

--- index.py
+++ index.py
@@ -1,9 +1,14 @@
+import random
+
 from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
+app.debug = True
+
+templates = ["tilt-shift", "voronoi"]
 
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('{}.html'.format(random.choice(templates)))
